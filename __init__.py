b6962d3fbeb64ca726a4cb53809d69829d04d6b8
Mario Barboza <90014477+marioBarbozaA@users.noreply.github.com>
2024-02-17 21:20:24 -0600

    Can import TicTacToe succesfully

---
+++ __init__.py
@@ -0,0 +1,13 @@
+#import tictactoe
+from controller.TicTacToe import TicTacToe as gato
+#create a main function to run the program
+def main():
+    
+    print("Hello World!")
+    print("This is the main function.")
+
+if __name__ == "__main__":
+    main()
+    gato("hola","mundo").play_game()
+
+
--- controller/TicTacToe.py
+++ controller/TicTacToe.py
@@ -1,4 +1,4 @@
-from ..model import BotInterface
+from model import BotInterface
 class TicTacToe:
     def __init__(self, player1: BotInterface, player2: BotInterface):
         self.board = [[None for _ in range(3)] for _ in range(3)]
@@ -7,5 +7,6 @@ class TicTacToe:
         self.current_player = self.player1
 
     def play_game(self):
+        print("Soy Tic tac toe")
         # Lógica para jugar el juego, alternando entre los bots.
         pass
